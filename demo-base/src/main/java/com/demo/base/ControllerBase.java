dfd8d06f250f23b4d4eb05da76aee121eb48562e
ALI <1416978277@qq.com>
2024-12-10 11:44:10 +0800

    1210

--- demo-base/src/main/java/com/demo/base/ControllerBase.java
+++ demo-base/src/main/java/com/demo/base/ControllerBase.java
@@ -17,6 +17,8 @@ public class ControllerBase {
 
     /**
      * 请求参数错误
+     *
+     * @return 请求参数错误
      */
     public static Result paramError() {
         return Result.e(ResultCode.PARAM_ERROR);
@@ -26,6 +28,7 @@ public class ControllerBase {
      * 是null对象
      *
      * @param object 对象
+     * @return 是null对象
      */
     public static boolean isNull(Object object) {
         return object == null;
@@ -35,6 +38,7 @@ public class ControllerBase {
      * 存在null对象
      *
      * @param objects 多个对象
+     * @return 存在null对象
      */
     public static boolean existNull(Object... objects) {
         if (objects == null) {
@@ -52,6 +56,7 @@ public class ControllerBase {
      * 全是null对象
      *
      * @param objects 多个对象
+     * @return 全是null对象
      */
     public static boolean allNull(Object... objects) {
         if (objects == null) {
@@ -69,6 +74,7 @@ public class ControllerBase {
      * 是空字符串
      *
      * @param string 字符串
+     * @return 是空字符串
      */
     public static boolean isEmpty(String string) {
         return string == null || string.isEmpty();
@@ -78,6 +84,7 @@ public class ControllerBase {
      * 存在空字符串
      *
      * @param strings 多个字符串
+     * @return 存在空字符串
      */
     public static boolean existEmpty(String... strings) {
         if (strings == null) {
@@ -95,6 +102,7 @@ public class ControllerBase {
      * 全是空字符串
      *
      * @param strings 多个字符串
+     * @return 全是空字符串
      */
     public static boolean allEmpty(String... strings) {
         if (strings == null) {
@@ -112,6 +120,7 @@ public class ControllerBase {
      * 是空白字符串
      *
      * @param string 字符串
+     * @return 是空白字符串
      */
     public static boolean isBlack(String string) {
         return string == null || string.isBlank();
@@ -121,6 +130,7 @@ public class ControllerBase {
      * 存在空白字符串
      *
      * @param strings 多个字符串
+     * @return 存在空白字符串
      */
     public static boolean existBlack(String... strings) {
         if (strings == null) {
@@ -138,6 +148,7 @@ public class ControllerBase {
      * 全是空白字符串
      *
      * @param strings 多个字符串
+     * @return 全是空白字符串
      */
     public static boolean allBlack(String... strings) {
         if (strings == null) {
