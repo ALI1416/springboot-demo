102c0bc057df241d0375649d98937381afd01b0b
ALI <1416978277@qq.com>
2025-06-06 09:22:49 +0800

    0606

--- demo-mysql/sql/demo.sql
+++ demo-mysql/sql/demo.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80029
  Source Host           : localhost:3306
- Source Schema         : demo2
+ Source Schema         : demo
 
  Target Server Type    : MySQL
  Target Server Version : 80029
